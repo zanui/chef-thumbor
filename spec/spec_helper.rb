d30efe23292d2f7fcb8c8c105d8de409dd68bf89
Enrico Stahn <mail@enricostahn.com>
2013-12-17 01:16:33 +1100

    Move simplecov up to the top and add HTML formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+require 'simplecov'
+require 'coveralls'
+
+formatters = [SimpleCov::Formatter::HTMLFormatter]
+formatters << Coveralls::SimpleCov::Formatter
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
+SimpleCov.start
+
 require 'chefspec'
 require 'chefspec/berkshelf'
 require 'fauxhai'
 
-require 'coveralls'
-Coveralls.wear!

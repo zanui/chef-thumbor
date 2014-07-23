35a2a06480bf66a675907e327fe9ff11be46e94c
Enrico Stahn <mail@enricostahn.com>
2014-07-23 15:31:30 +1000

    Bump version to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@zanui.com.au'
 license          'Apache 2.0'
 description      'Installs and configures thumbor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 recipe 'thumbor::default', 'Installs thumbor from globocom Private Package Archive (PPA)'
 recipe 'thumbor::source', 'Installs thumbor from GitHub repository'
